3f9f39a15419b3c95b9fcd418d30e6f408b17ac2
kyneticDude <kynetictjabane@gmail.com>
2014-10-30 23:49:57 +0200

    Gunguluza Website
    Repo setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ktpGunguluzaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ktpGunguluzaApp
+ */
+angular.module('ktpGunguluzaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
